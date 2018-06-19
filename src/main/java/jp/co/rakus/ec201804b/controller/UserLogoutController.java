0ba91ddc8e6612897a7523cc5b4dea55c12c2a1b
sasaki.nanami <nanami.sasaki@rakus.co.jp>
2018-06-19 09:47:31 +0900

    ログアウトコントローラー完成

--- src/main/java/jp/co/rakus/ec201804b/controller/UserLogoutController.java
+++ src/main/java/jp/co/rakus/ec201804b/controller/UserLogoutController.java
@@ -2,14 +2,32 @@ package jp.co.rakus.ec201804b.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import jp.co.rakus.ec201804b.domain.User;
+
+/**
+ * ログアウト関連処理を行うコントローラー.
+ * @author Nanami.Sasaki
+ *
+ */
 @Controller
-@RequestMapping(value="/logout")
+@RequestMapping(value = "/logout")
+@SessionAttributes("user")
 public class UserLogoutController {
-	
-	@RequestMapping(value="/")
-	public String logout() {
-		return "/";
+
+	/**
+	 * セッション情報に含まれるMemberオブジェクトをクリアします.
+	 * @param member Sessionに入っているメンバー情報
+	 * @param sessionStatus　セッションステータス
+	 * @return　ログイン画面
+	 */
+	@RequestMapping(value = "sessionInvalidate")
+	public String sessionInvalidate(User user
+			, SessionStatus sessionStatus) {
+		sessionStatus.setComplete();
+		return "redirect:/index";
 	}
 
 }
