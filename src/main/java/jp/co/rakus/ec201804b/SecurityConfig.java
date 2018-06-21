9fdbec6578ee39593fbd58bb0f4540e721470be3
atsushi.sato <atsuhsi.sato@rakus.co.jp>
2018-06-21 12:08:22 +0900

    管理者フィルタ

--- src/main/java/jp/co/rakus/ec201804b/SecurityConfig.java
+++ src/main/java/jp/co/rakus/ec201804b/SecurityConfig.java
@@ -93,8 +93,8 @@ public class SecurityConfig {
 		protected void configure(HttpSecurity http) throws Exception {
 
 			http.antMatcher("/admin/**").authorizeRequests() // 認可に関する設定
-					.antMatchers("/**/**").permitAll() // 「/」などのパスは全てのユーザに許可
-					.antMatchers("/admin/**").hasRole("ADMIN") //
+					.antMatchers("/admin/index").permitAll() // 「/」などのパスは全てのユーザに許可
+					.antMatchers("/admin/form", "/admin/menu","/admin/viewItemInsert","/admin/viewList","/admin/orderList", "/admin/orderDetail**").hasRole("ADMIN") //
 					// /admin/から始まるパスはADMIN権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
 					// .antMatchers("/member/**").hasRole("MEMBER") //
 					// /member/から始まるパスはMEMBER権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
