9ea2800c8555047d843ab349f2a007624950db86
atsushi.sato <atsuhsi.sato@rakus.co.jp>
2018-06-27 08:49:08 +0900

    合算完成

--- src/main/java/jp/co/rakus/ec201804b/controller/ShowShoppingCartController.java
+++ src/main/java/jp/co/rakus/ec201804b/controller/ShowShoppingCartController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import jp.co.rakus.ec201804b.domain.LoginUser;
 import jp.co.rakus.ec201804b.domain.Order;
 import jp.co.rakus.ec201804b.domain.OrderItem;
+import jp.co.rakus.ec201804b.repository.OrderItemRepository;
 import jp.co.rakus.ec201804b.repository.OrderRepository;
 
 @Controller
@@ -22,6 +23,9 @@ public class ShowShoppingCartController {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Autowired
 	private HttpSession session;
@@ -45,9 +49,27 @@ public class ShowShoppingCartController {
 		Order viewOrder = orderRepository.findByUserIdAndStatus(userId, 0);
 		if(viewOrder !=null) {
 			Long orderId = viewOrder.getId();
-
+			
+			
+			//合算と更新と削除
+			List<OrderItem> orderItemList = orderItemRepository.findByOrderId(orderId);
+			for (OrderItem orderItem : orderItemList) {
+				Long itemId = orderItem.getItemId();
+				Long orderItemOrderId = orderItem.getOrderId();
+				List<OrderItem> orderItemLi = orderItemRepository.findByItemIdAndOrderId(itemId,orderItemOrderId);
+				
+				if(orderItemLi.size() >1) {
+					Long itemIdzero = orderItemLi.get(0).getItemId();
+					Long orderIdzero = orderItemLi.get(0).getOrderId();
+					//Long zeroId = orderItemLi.get(0).getId();//1つ目の商品のid
+					Long oneId  = orderItemLi.get(1).getId();//2つ目の商品のid
+					Integer newQuantity = orderItemLi.get(0).getQuantity()+orderItemLi.get(1).getQuantity();//合算処理
+					orderItemRepository.updateById(itemIdzero,orderIdzero,newQuantity);//1つ目に合算した個数をupdate
+					orderItemRepository.deleteById(oneId);//2つ目を削除
+				}
+			}
+//			//List<OrderItem> orderItemList = orderItemRepository.findByItemId(itemId);
 			order=orderRepository.load(orderId);
-
 		}
 		
 //		for (Order order : orderList) {
